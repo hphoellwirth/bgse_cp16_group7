a66e2ad6377f312f62d6fdaf968addeb77d9240c
v-veronika <veronika.kyuchukova@barcelonagse.eu>
2016-11-27 00:07:26 +0100

    corrected Interpolation fila-no negative pollution anymore

--- analysis/preInterpolatePopulation.R
+++ analysis/preInterpolatePopulation.R
@@ -52,6 +52,9 @@ invisible(dbGetQuery(dbConn, "set names utf8"))
 # ----------------------------------------------------------------------
 cityIDs <- dbGetQuery(dbConn, "SELECT cityID FROM cityPopulation group by cityID;")
 
+end.year<- dbGetQuery(dbConn, "SELECT max(year) FROM cityPopulation;")
+end_year<- as.numeric(end.year)
+print(end_year)
 
 # #min number of data points needed to interpolate/extrapolate the data
 min.data=2
@@ -69,7 +72,7 @@ for (i in 1:length(cityIDs[ ,1])) {
   start_year<- as.numeric(start.year)
   print(start_year)
 
-  year<-seq(start_year, 2013)
+  year<-seq(start_year, end_year)
   #create a data frame with the years and data needed for the intrapolation
   popul_data<-as.data.frame(year)
   inserts <- c()
@@ -79,17 +82,19 @@ for (i in 1:length(cityIDs[ ,1])) {
     popul_data$population<-NA
     popul_data$cityID<-my_data$cityID[1]
     #insert the data from the observations present
-    popul_data$population[match(my_data$year[my_data$year<2014], popul_data$year)]<-my_data$population[my_data$year<2014]
+    popul_data$population[match(my_data$year, popul_data$year)]<-my_data$population
     #interpolate the data 
     popul_data$population<-na.spline(popul_data$population)
-    print(match(my_data$year[my_data$year<2014], popul_data$year))
-    import.data<-popul_data[-match(my_data$year[my_data$year<2014], popul_data$year), ]
+    print(match(my_data$year, popul_data$year))
+    import.data<-popul_data[-match(my_data$year, popul_data$year), ]
     print(import.data)
   }
 
   if (nrow(import.data)>0) {
     for (j in 1:nrow(import.data)){
+      if (import.data$year[j]<= 2013){
       inserts <- c(inserts, paste0("(\"", import.data$cityID[j], "\",", import.data$year[j] , ",", import.data$population[j], ", true )"))
+      }
     }
   }
   
@@ -109,6 +114,10 @@ for (i in 1:length(cityIDs[ ,1])) {
 
 countryIDs <- dbGetQuery(dbConn, "SELECT countryID FROM countryPopulation group by countryID;")
 
+end.year<- dbGetQuery(dbConn, "SELECT max(year) FROM countryPopulation;")
+end_year<- as.numeric(end.year)
+print(end_year)
+
 # #min number of data points needed to interpolate/extrapolate the data
 min.data=2
 
@@ -126,7 +135,7 @@ for (i in 1:length(countryIDs[ ,1])) {
   print(start_year)
   
 #create a data frame with the years and data needed fo the interpolation
-  year<-seq(start_year, 2013)
+  year<-seq(start_year, end_year)
   popul_data<-as.data.frame(year)
   inserts <- c()
   if (length(my_data$year) >= min.data) {
@@ -135,17 +144,19 @@ for (i in 1:length(countryIDs[ ,1])) {
     popul_data$population<-NA
     popul_data$countryID<-my_data$countryID[1]
     #insert the data from the observations present
-    popul_data$population[match(my_data$year[my_data$year<2014], popul_data$year)]<-my_data$population[my_data$year<2014]
+    popul_data$population[match(my_data$year, popul_data$year)]<-my_data$population
     #interpolate the data 
     popul_data$population<-na.spline(popul_data$population)
-    print(match(my_data$year[my_data$year<2014], popul_data$year))
-    import.data<-popul_data[-match(my_data$year[my_data$year<2014], popul_data$year), ]
+    print(match(my_data$year, popul_data$year))
+    import.data<-popul_data[-match(my_data$year, popul_data$year), ]
     print(import.data)
   }
 
   if (nrow(import.data)>0) {
     for (j in 1:nrow(import.data)){
+      if (import.data$year[j]<=2013){
       inserts <- c(inserts, paste0("(\"", import.data$countryID[j], "\",", import.data$year[j] , ",", import.data$population[j], ", true )"))
+      }
     }
   }
   
